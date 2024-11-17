0d2523b8d70e365e9890ece99d693357213af10c
Sagar Johari <joharisagar53@gmail.com>
2024-11-18 01:17:45 +0530

    added new button

--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import PrimeVue from 'primevue/config';
+import Aura from '@primevue/themes/aura';
 import App from './App.vue'
 import router from './router'
+import Button from "primevue/button"
 
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura
+    }
+});
+app.component('Button', Button);
 app.use(router)
 
 app.mount('#app')
